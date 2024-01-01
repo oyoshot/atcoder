2376f06a594f4b57212d3b2bdf28b63ba72da5af
oyoshot <oyoshot@gmail.com>
2024-01-01 14:56:23 +0900

    d: エラトステネスのふるいと累積和

--- abc084/src/bin/d.rs
+++ abc084/src/bin/d.rs
@@ -1,5 +1,44 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
-    input! {}
+    input! {
+        q: usize
+    }
+
+    // エラトステネスのふるい
+    let is_primes = eratosthenes(100_000);
+
+    // 累積和
+    let mut com_sum = vec![0; 100_001];
+    for i in 1..com_sum.len() {
+        if i % 2 == 1 && is_primes[i] && is_primes[(i + 1) / 2] {
+            com_sum[i] = 1;
+        }
+    }
+    for i in 1..com_sum.len() {
+        com_sum[i] += com_sum[i - 1];
+    }
+
+    for _ in 0..q {
+        input! {
+            l: usize, r:usize,
+        }
+
+        println!("{}", com_sum[r] - com_sum[l - 1]);
+    }
+}
+
+fn eratosthenes(n: usize) -> Vec<bool> {
+    let mut is_prime = vec![true; n + 1];
+    is_prime[1] = false;
+    for i in 1..=n {
+        if !is_prime[i] {
+            continue;
+        }
+        for i in (i * 2..=n).step_by(i) {
+            is_prime[i] = false;
+        }
+    }
+    is_prime
 }
