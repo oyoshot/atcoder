07b1ba7f8e56729ed5388c6fe8de2be08466a219
oyoshot <oyoshot@gmail.com>
2024-01-02 17:43:13 +0900

    条件を満足する区間を累積和使って数えるやつ

--- agc023/src/bin/a.rs
+++ agc023/src/bin/a.rs
@@ -1,5 +1,31 @@
-use proconio::input;
+use proconio::{fastout, input};
+use std::collections::HashMap;
 
+#[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut cum_sum = vec![0; n + 1];
+    for i in 0..n {
+        cum_sum[i + 1] += cum_sum[i] + a[i];
+    }
+    //dbg!(&cum_sum);
+
+    let mut map = HashMap::new();
+    for i in 0..n + 1 {
+        *map.entry(cum_sum[i]).or_insert(0 as i64) += 1;
+    }
+    //dbg!(&map);
+
+    let mut ans = 0;
+    for (_, v) in map {
+        //dbg!(v * (v - 1) / 2);
+
+        // nC2
+        ans += v * (v - 1) / 2;
+    }
+    println!("{}", ans);
 }
