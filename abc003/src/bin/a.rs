8b79f0e4b18fbd9669eec06feb1e845582fcad3d
oyoshot <oyoshot@gmail.com>
2024-01-07 12:48:58 +0900

    やった

--- abc003/src/bin/a.rs
+++ abc003/src/bin/a.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! { n: usize, }
+
+    let mut ans = 0;
+    for i in 1..=n {
+        ans += i * 10000;
+    }
+    println!("{}", ans / n);
 }
--- abc003/src/bin/c.rs
+++ abc003/src/bin/c.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize, k: usize,
+        mut r: [usize; n],
+    }
+
+    r.sort();
+
+    let mut ans = 0.0;
+    for i in n - k..n {
+        ans = (ans + r[i] as f64) / 2.0;
+    }
+    println!("{:.9}", ans);
 }
