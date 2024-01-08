b63afa3149d13eb7c27329096791236eaaffa36b
oyoshot <oyoshot@gmail.com>
2024-01-08 11:34:11 +0900

    AC だけどローカルテストは通らなかった

--- abc004/src/bin/b.rs
+++ abc004/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! { c: [[char; 4]; 4]}
+
+    for i in 0..c.len() {
+        for j in 0..c[i].len() {
+            print!(
+                "{}{}",
+                c[c.len() - 1 - i][c[i].len() - 1 - j],
+                if j != c[i].len() { " " } else { "" }
+            );
+        }
+        println!();
+    }
 }
