10c62f84b82cd72377d04276be70a332ef09a7eb
oyoshot <oyoshot@gmail.com>
2024-01-13 12:27:41 +0900

    やった

--- abc127/src/bin/b.rs
+++ abc127/src/bin/b.rs
@@ -2,5 +2,12 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        r: usize, d: usize, x_2000: usize,
+    };
+    let mut x = x_2000;
+    for _ in 1..=10 {
+        x = r * x - d;
+        println!("{}", x);
+    }
 }
