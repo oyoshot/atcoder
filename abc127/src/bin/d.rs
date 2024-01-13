98fea7bdf27afc5d205c9270f6ddcd82b1824cd3
oyoshot <oyoshot@gmail.com>
2024-01-13 12:25:30 +0900

    ランレングス圧縮で表現したカードを全部混ぜてソートして総和の最大数を求めた。

--- abc127/src/bin/d.rs
+++ abc127/src/bin/d.rs
@@ -1,5 +1,3 @@
-use std::collections::BinaryHeap;
-
 use proconio::{fastout, input};
 
 #[fastout]
@@ -10,16 +8,44 @@ fn main() {
         bc: [(usize, usize); m],
     };
 
-    let mut heap = BinaryHeap::from(a);
-    for &(b, c) in &bc {
-        for _ in 0..b {
-            heap.push(c);
-        }
+    let mut heap = vec![];
+    for &a in &a {
+        heap.push((1, a as i64));
     }
+    for &(b, c) in &bc {
+        heap.push((b as i64, c as i64));
+    }
+    heap.sort_by(|a, b| a.1.cmp(&b.1));
 
     let mut ans = 0;
     for _ in 0..n {
-        ans += heap.pop().unwrap();
+        let (b, c) = heap.pop().unwrap();
+        ans += c;
+        if b > 1 {
+            heap.push((b - 1, c));
+        }
     }
     println!("{}", ans);
 }
+
+// TLE
+//fn main() {
+//    input! {
+//        n: usize, m: usize,
+//        a: [usize; n],
+//        bc: [(usize, usize); m],
+//    };
+//
+//    let mut heap = BinaryHeap::from(a);
+//    for &(b, c) in &bc {
+//        for _ in 0..b {
+//            heap.push(c);
+//        }
+//    }
+//
+//    let mut ans = 0;
+//    for _ in 0..n {
+//        ans += heap.pop().unwrap();
+//    }
+//    println!("{}", ans);
+//}
