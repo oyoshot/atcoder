d8d9abbb0238e4bd12ccc2c0043243ac5b35c920
oyoshot <oyoshot@gmail.com>
2024-01-13 10:26:43 +0900

    tle

--- abc127/src/bin/d.rs
+++ abc127/src/bin/d.rs
@@ -1,6 +1,25 @@
+use std::collections::BinaryHeap;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        n: usize, m: usize,
+        a: [usize; n],
+        bc: [(usize, usize); m],
+    };
+
+    let mut heap = BinaryHeap::from(a);
+    for &(b, c) in &bc {
+        for _ in 0..b {
+            heap.push(c);
+        }
+    }
+
+    let mut ans = 0;
+    for _ in 0..n {
+        ans += heap.pop().unwrap();
+    }
+    println!("{}", ans);
 }
