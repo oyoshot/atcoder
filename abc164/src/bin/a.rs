3bd674ee206ee55dff0a72cb45efc54cfb7f5947
oyoshot <oyoshot@gmail.com>
2024-01-18 01:24:04 +0900

    やった

--- abc164/src/bin/a.rs
+++ abc164/src/bin/a.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        s: usize, w: usize,
+    };
+
+    println!("{}", if w >= s { "unsafe" } else { "safe" });
 }
--- abc164/src/bin/b.rs
+++ abc164/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        mut a: i64,  b: i64, mut  c: i64,  d: i64,
+    };
+
+    while a > 0 && c > 0 {
+        c -= b;
+        if c <= 0 {
+            println!("Yes");
+            return;
+        }
+        a -= d;
+    }
+    println!("No");
 }
