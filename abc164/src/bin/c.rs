847f8035cb800f20f0ffc077abb7f3b962a67fec
oyoshot <oyoshot@gmail.com>
2024-01-20 15:43:21 +0900

    やった

--- abc164/src/bin/c.rs
+++ abc164/src/bin/c.rs
@@ -1,6 +1,13 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        n:usize,
+        s: [String; n],
+    };
+
+    let ans = s.iter().unique().count();
+    println!("{}", ans);
 }
