a6955762b416b5f9064541e808105e9a6f4ea93d
oyoshot <oyoshot@gmail.com>
2024-01-21 15:18:11 +0900

    閉区間の両端が等しいのを条件に 2019 の倍数を数える

--- abc084/src/bin/a.rs
+++
@@ -1,9 +0,0 @@
-use proconio::input;
-
-fn main() {
-    input! {
-        m:usize
-    }
-
-    println!("{}", (24 + 24) - m);
-}
--- abc164/src/bin/d.rs
+++ abc164/src/bin/d.rs
@@ -1,6 +1,26 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
+
+const MOD: usize = 2019;
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {s: Chars};
+
+    let mut num = 0;
+    let mut d = 1;
+    let mut counts = vec![0usize; MOD];
+    counts[num] += 1;
+    for &v in s.iter().rev()  {
+        let add = v.to_digit(10).unwrap() as usize;
+        num = (num + add * d) % MOD;
+        d = (d * 10) % MOD;
+        //dbg!(num);
+        counts[num] += 1;
+    }
+
+    let mut ans = 0;
+    for count in counts {
+        ans += count * count.saturating_sub(1) / 2;
+    }
+    println!("{}", ans);
 }
