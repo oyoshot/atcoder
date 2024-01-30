1bad0cd37a81bcf07c6fe689f6a2602266e9b567
oyoshot <oyoshot@gmail.com>
2024-01-31 00:09:35 +0900

    やった
    fmt

--- abc102/src/bin/b.rs
+++ abc102/src/bin/b.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        mut a: [usize; n]
+    };
+
+    a.sort();
+    println!("{}", a[a.len() - 1] - a[0]);
 }
