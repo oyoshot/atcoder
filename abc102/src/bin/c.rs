c5953a1b0fb7cdd26cfbf4378a144d87c3862134
oyoshot <oyoshot@gmail.com>
2024-03-02 16:27:30 +0900

    マンハッタン距離の差の総和を最小化する時はメディアン

--- abc102/src/bin/c.rs
+++ abc102/src/bin/c.rs
@@ -1,6 +1,17 @@
+use std::i64;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        n : usize,
+        a : [i64; n]
+    };
+
+    let mut aa: Vec<_> = a.iter().enumerate().map(|(i, &v)| v - i as i64).collect();
+    aa.sort();
+    let mp = aa[n / 2];
+    let ans = aa.iter().map(|v| (mp - v).abs()).sum::<i64>();
+    println!("{}", ans);
 }
