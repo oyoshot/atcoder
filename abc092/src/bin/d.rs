440c9d3924f20a922d1516a413e8ea2d125a21c0
oyoshot <oyoshot@gmail.com>
2024-03-09 17:40:29 +0900

    グリッドを白と黒に分割して 1 マス飛ばしで色をつける発想

--- abc092/src/bin/d.rs
+++ abc092/src/bin/d.rs
@@ -1,6 +1,43 @@
 use proconio::{fastout, input};
 
+const H: usize = 100;
+const W: usize = 100;
+
 #[fastout]
 fn main() {
-    input! {};
+    input! { a: usize, b: usize,};
+
+    let mut grid = vec![['.'; W]; H];
+    for i in 0..H / 2 {
+        for j in 0..W {
+            grid[i][j] = '#';
+        }
+    }
+
+    let mut row = 0;
+    let mut col = 0;
+    for _ in 0..a - 1 {
+        if col >= W {
+            row += 2;
+            col = 0;
+        }
+        grid[row][col] = '.';
+        col += 2;
+    }
+
+    row = 51;
+    col = 0;
+    for _ in 0..b - 1 {
+        if col >= W {
+            row += 2;
+            col = 0;
+        }
+        grid[row][col] = '#';
+        col += 2;
+    }
+
+    println!("{} {}", H, W);
+    for v in grid {
+        println!("{}", v.iter().collect::<String>());
+    }
 }
