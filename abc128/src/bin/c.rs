e909a179415d53c9ec69ca87be7990569dbb19e3
oyoshot <oyoshot@gmail.com>
2024-03-31 17:50:00 +0900

    Bit 全探索

--- abc128/src/bin/c.rs
+++ abc128/src/bin/c.rs
@@ -1,6 +1,29 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {};
+    input! {
+        n: usize, m: usize,
+        s: [[Usize1]; m],
+        p: [usize; m],
+    };
+
+    //dbg!(&s);
+    //dbg!(&p);
+    let mut ans = 0;
+    for bit in 0..(1 << n) {
+        let mut sw = vec![0; m];
+        for i in 0..m {
+            for &s in &s[i] {
+                if bit >> s & 1 == 1 {
+                    sw[i] ^= 1;
+                }
+            }
+        }
+
+        if sw == p {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
